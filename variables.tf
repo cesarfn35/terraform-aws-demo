3ce0aaa55afef7a41a1ab418cd70544b2d2bb93e
Cesar Fernandez <cesar.fernandez@hashicorp.com>
2024-05-20 16:05:55 +0200

    Edited variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "region" {
   type = string
   description = "AWS region"
-  default     = "eu-west-1"
 }
 
 variable "ssh_key" {
@@ -12,12 +11,10 @@ variable "ssh_key" {
 variable "instance_type" {
   type = string
   description = "EC2 instance type"
-  default     = "t2.micro"
 }
 
 variable "instance_name" {
   type = string
   description = "EC2 instance name"
-  default     = "terraform"
 }
 
